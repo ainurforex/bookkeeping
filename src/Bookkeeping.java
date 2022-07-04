45c3f9fe6446f3097a3811dff5e7f1d14bcb1385
Ainur <ainurforex@mail.ru>
2022-07-04 20:57:00 +0500

    Курсовая Базовая Сложность

--- src/Bookkeeping.java
+++ src/Bookkeeping.java
@@ -1,5 +1,143 @@
 public class Bookkeeping {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] employeeBase = new Employee[10];
+        employeeBase[0] = new Employee("Петров", "Иван", "Сидорович", 1, 12000);
+        employeeBase[1] = new Employee("Нечаев", "Петр", "Васильевич", 1, 13000);
+        employeeBase[2] = new Employee("Сидоров", "Василий", "Петрович", 1, 14000);
+        employeeBase[3] = new Employee("Бекетова", "Анна", "Васильевнв", 2, 15000);
+        employeeBase[4] = new Employee("Зарев", "Михаил", "Артурович", 2, 21000);
+        employeeBase[5] = new Employee("Пушкин", "Александр", "Сергеевич", 3, 22000);
+        employeeBase[6] = new Employee("Сиськин", "Гиви", "Карэнович", 3, 23000);
+        employeeBase[7] = new Employee("Могелева", "Кристина", "Аедреевна", 4, 24000);
+        System.out.println(dataOfEmployees(employeeBase));
+        System.out.println(summSalary(employeeBase));
+        System.out.println(findEmployeesMinSalary(employeeBase));
+        System.out.println(findEmployeesMaxSalary(employeeBase));
+        System.out.println(avarageSalary(employeeBase));
+        System.out.println(listOfEmployees(employeeBase));
+
     }
+
+    private static String dataOfEmployees(Employee[] employees) {
+        if (isEmptyBase(employees)) {
+            return "None";
+        }
+
+        String dataOfEmployees = "";
+
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                dataOfEmployees += employees[i] + "\n";
+            }
+        }
+
+        return dataOfEmployees;
+    }
+
+
+    private static String listOfEmployees(Employee[] employees) {
+        if (isEmptyBase(employees)) {
+            return "None";
+        }
+
+        String listOfEmployees = "";
+
+        for (int i = 0; i < employees.length; i++) {
+
+            if (employees[i] != null) {
+                listOfEmployees += employees[i].getFullName() + "\n";
+            }
+        }
+
+        return listOfEmployees;
+    }
+
+    private static int summSalary(Employee[] employees) {
+        if (isEmptyBase(employees)) {
+            return 0;
+        }
+
+        int summSalary = 0;
+
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                summSalary += employees[i].getSalary();
+            }
+        }
+
+        return summSalary;
+    }
+
+    private static String findEmployeesMinSalary(Employee[] employees) {
+        if (isEmptyBase(employees)) {
+            return "None";
+        }
+
+        int minSalary = employees[0].getSalary();
+        int indexEmployeesMinSalary = 0;
+
+        for (int i = 1; i < employees.length; i++) {
+            if (employees[i] != null) {
+                if (employees[i].getSalary() < minSalary) {
+                    minSalary = employees[i].getSalary();
+                    indexEmployeesMinSalary = i;
+                }
+            }
+        }
+
+        return employees[indexEmployeesMinSalary].getFullName();
+    }
+
+    private static String findEmployeesMaxSalary(Employee[] employees) {
+        if (isEmptyBase(employees)) {
+            return "None";
+        }
+
+        int maxSalary = employees[0].getSalary();
+        int indexEmployeesMaxSalary = 0;
+
+        for (int i = 1; i < employees.length; i++) {
+            if (employees[i] != null) {
+                if (employees[i].getSalary() > maxSalary) {
+                    maxSalary = employees[i].getSalary();
+                    indexEmployeesMaxSalary = i;
+                }
+            }
+        }
+
+        return employees[indexEmployeesMaxSalary].getFullName();
+    }
+
+    private static int avarageSalary(Employee[] employees) {
+        if (isEmptyBase(employees)) {
+            return 0;
+        }
+
+        int countEmployees = 0;
+        int summSalary = summSalary(employees);
+
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                countEmployees++;
+            }
+        }
+
+        return Math.round(summSalary / countEmployees);
+    }
+
+    private static boolean isEmptyBase(Employee[] employees) {
+        int countEmployees = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                countEmployees++;
+            }
+        }
+        if (countEmployees == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
---
+++ src/Employee.java
@@ -0,0 +1,65 @@
+public class Employee {
+    private static int idCount = 0;
+    private int id;
+    private int department;
+    private String surname;
+    private String name;
+    private String middleName;
+    private int salary;
+
+    public Employee(String surname, String name, String middleName, int department, int salary) {
+        this.id = idIncrease();
+        this.department = department;
+        this.surname = surname;
+        this.name = name;
+        this.middleName = middleName;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getFullName() {
+        return surname + ' ' + name + ' ' + middleName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "id " + id + ',' + " отдел " + department + ',' + " Ф.И.О. " + surname + ' '
+                + name + ' ' + middleName + ',' + " зарплата " + salary + '.';
+    }
+
+    private int idIncrease() {
+        return ++idCount;
+    }
+
+}
